0f49d1cf6e14f0003cdce2912fdac763ad25c967
Rishi Giri <rushforlinux@gmail.com>
2015-05-29 16:20:23 +0530

    Create combinations.js

---
+++ combinations.js
@@ -0,0 +1,31 @@
+// combination technique
+
+(function (exports) {
+  'use strict';
+
+  var combinations = (function () {
+    var res = [];
+
+    function combinations(arr, k, start, idx, current) {
+      if (idx === k) {
+        res.push(current.slice());
+        return;
+      }
+      for (var i = start; i < arr.length; i += 1) {
+        current[idx] = arr[i];
+        combinations(arr, k, i + 1, idx + 1, current);
+      }
+    }
+    return function (arr, k) {
+      res = [];
+      combinations(arr, k, 0, 0, []);
+      var temp = res;
+      // Free the extra memory
+      res = null;
+      return temp;
+    };
+  }());
+
+  exports.combinations = combinations;
+
+}((typeof window === 'undefined') ? module.exports : window));
